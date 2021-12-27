79c2b88e64d7877309cc0eadc0336461e3255b4c
zzw918 <zhuzhenwei789@gmail.com>
2021-12-27 18:14:07 +0800

    init

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" style={{ margin: 100 }}>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>
+        hi hedy
+      </h1>
     </div>
   );
 }
